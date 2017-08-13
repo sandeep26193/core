198904df8d6a7c9ee9c89f179dce971f4d332f73
Lavanya <Lavanya@192.168.1.12>
2017-08-13 15:47:08 -0500

    psuedocode and tests for perfectfactors

--- src/main/java/com/enterprise/core/PerfectFactors.java
+++ src/main/java/com/enterprise/core/PerfectFactors.java
@@ -2,9 +2,18 @@ package com.enterprise.core;
 
 public class PerfectFactors {
 
-	public int[] find(int i) {
-		// TODO Auto-generated method stub
+	public int[] find(int number) {
+		// check if number is perfect exponent of a number less that 9
+		//eg: number = 32 , return int[]{2,5} since 2^5 = 32
+		//eg: 36, return int[]{6,2}, since 6^2 = 36
+		 //eg: 64, return either int[]{2,6} or int[]{8,2}
+		//eg: 121, return null, even thought 121 = 11^2, 11 is greaterthan 9
+		//eg: 40, return null
+		
 		return null;
+
 	}
+
+	
 
 }
--- src/test/java/com/enterprise/core/PerfectFactorsTest.java
+++ src/test/java/com/enterprise/core/PerfectFactorsTest.java
@@ -10,14 +10,40 @@ PerfectFactors perfectFactors = new PerfectFactors();
 	@Test
 	public void testFactors() {
 		int[] factors = perfectFactors.find(36);
-		assertEquals(6,factors[0]);
-		assertEquals(2,factors[1]);
+		
+		assertEquals(36,findFactor(factors));
 	}
+	
+	@Test
+	public void testFactors_of2() {
+		int[] factors = perfectFactors.find(32);
+		
+		assertEquals(32,findFactor(factors));
+	}	
 	
 	@Test
 	public void testFactors_notPerfect() {
 		int[] factors = perfectFactors.find(35);
 		assertNull(factors);
+	}
+	@Test
+	public void testFactors_notPerfectEvent() {
+		int[] factors = perfectFactors.find(40);
+		assertNull(factors);
+	}
+	
+	@Test
+	public void testFactors_morethanoneanswer() {
+		int[] factors = perfectFactors.find(64);
+		assertEquals(64,findFactor(factors));
+	}
+	
+	private int findFactor(int[] factors) {
+		int factor = 1;
+		for(int i = 0; i < factors[1]; i++) {
+			factor = factor * factors[0];
+		}
+		return factor;
 	}
 
 }
